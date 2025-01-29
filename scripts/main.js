220f356386df06d35dd25b40cb08910d54bb5cfc
jaafaralireeabi <159414760+jaafaralireeabi@users.noreply.github.com>
2025-01-29 17:37:44 +0300

    commit-1

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Scroll to Top Script
   const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
@@ -15,21 +14,3 @@
   scrollToTopBtn.addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
-=======
-// Scroll to Top Script
-  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
-
-  // Show the button when scrolled down 100px
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 100) {
-      scrollToTopBtn.style.display = "flex";
-    } else {
-      scrollToTopBtn.style.display = "none";
-    }
-  });
-
-  // Scroll to top when button is clicked
-  scrollToTopBtn.addEventListener("click", () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  });
->>>>>>> 5b9d6e8a5eec659ae907396a29f53b82ddab4a20
